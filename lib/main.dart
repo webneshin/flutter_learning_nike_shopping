9cf95e8e5f209978a6536baf934250e540f54eb6
webneshin <sajjad.eb@gmail.com>
2023-08-07 00:15:27 +0330

    create custome theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_learning_nike_shopping/theme.dart';
 
 void main() {
   runApp(const MainApp());
@@ -7,12 +9,50 @@ void main() {
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
+
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    const FlexScheme colorFlexScheme = FlexScheme.sanJuanBlue;
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: FlexThemeData.light(
+        scheme: colorFlexScheme,
+        fontFamily: 'yekan',
+      ),
+      // darkTheme: darkTheme,
+      themeMode: ThemeMode.light,
+      home: const Directionality(
+          textDirection: TextDirection.rtl, child: HomeScreen()),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("فروشگاه نایک"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(
+          Icons.add,
+          color: Theme.of(context).colorScheme.onPrimary,
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'سلام سلام جهان من!',
+            ),
+            TextButton(onPressed: () {}, child: Text("ثبت سفارش"))
+          ],
         ),
       ),
     );
---
+++ lib/theme.dart
@@ -0,0 +1,6 @@
+import 'package:flex_color_scheme/flex_color_scheme.dart';
+import 'package:flutter/material.dart';
+
+
+
+
