f4e14389faef648d1de90b1ed6a14a16b3c184eb
webneshin <sajjad.eb@gmail.com>
2023-08-09 02:52:16 +0330

    add products to home

--- lib/common/utils.dart
+++ lib/common/utils.dart
@@ -1,3 +1,7 @@
 import 'package:flutter/cupertino.dart';
 
 const defaultScrollPhysics = BouncingScrollPhysics();
+
+extension PriceLabel on int {
+  String get withPriceLable =>'$this تومان';
+}
--- lib/ui/home/home.dart
+++ lib/ui/home/home.dart
@@ -1,8 +1,8 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_learning_nike_shopping/common/utils.dart';
 import 'package:flutter_learning_nike_shopping/data/banner.dart';
+import 'package:flutter_learning_nike_shopping/data/product.dart';
 import 'package:flutter_learning_nike_shopping/data/repo/banner_repository.dart';
 import 'package:flutter_learning_nike_shopping/data/repo/product_repository.dart';
 import 'package:flutter_learning_nike_shopping/generated/assets.dart';
@@ -39,18 +39,36 @@ class HomeScreen extends StatelessWidget {
                         return Container(
                           height: 56,
                           alignment: Alignment.center,
-
                           child: Image.asset(
-                            Assets.imgNikeLogo,height: 24,
+                            Assets.imgNikeLogo,
+                            height: 24,
                             fit: BoxFit.fitHeight,
                           ),
                         );
                       case 2:
-                        return BannerSliderShow(banners: state.banners,);
+                        return BannerSliderShow(
+                          banners: state.banners,
+                        );
+                      case 3:
+                        return _HorizontalProductList(
+                          title: 'جدیدترین',
+                          onTap: () {},
+                          products: state.latestProducts,
+                        );
+                      case 4:
+                        return _HorizontalProductList(
+                          title: 'پربازدیدترین',
+                          onTap: () {},
+                          products: state.popularProducts,
+                        );
                       default:
                         return Container(
-                          color: Colors.green,
-                          child: Text((index + 1).toString()),
+                          color: Theme.of(context).colorScheme.primary,
+                          child: Text(
+                            (index + 1).toString(),
+                            style: TextStyle(
+                                color: Theme.of(context).colorScheme.onPrimary),
+                          ),
                         );
                     }
                   },
@@ -86,3 +104,95 @@ class HomeScreen extends StatelessWidget {
   }
 }
 
+class _HorizontalProductList extends StatelessWidget {
+  final String title;
+  final GestureTapCallback onTap;
+  final List<Product> products;
+
+  const _HorizontalProductList({
+    super.key,
+    required this.title,
+    required this.products,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 12, right: 12),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(title),
+              TextButton(onPressed: () => onTap, child: const Text('نمایش همه'))
+            ],
+          ),
+        ),
+        SizedBox(
+          height: 290,
+          child: ListView.builder(
+            physics: defaultScrollPhysics,
+            itemCount: products.length,
+            scrollDirection: Axis.horizontal,
+            padding: const EdgeInsets.only(left: 8, right: 8),
+            itemBuilder: (BuildContext context, int index) {
+              final product = products[index];
+              return Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: SizedBox(
+                  width: 176,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Stack(
+                        children: [
+                          SizedBox(
+                            width: 176,
+                            height: 189,
+                            child: ImageLoadingService(
+                              imageUrl: product.imageUrl,
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                          ),
+                          IconButton(
+                            onPressed: () {},
+                            icon: const Icon(
+                              Icons.favorite_border,
+                            ),
+                            splashColor: Colors.transparent,
+                            highlightColor: Colors.transparent,
+                            focusColor: Colors.transparent,
+                            hoverColor: Colors.transparent,
+                            iconSize: 20,
+                            splashRadius: 1,
+                            tooltip: 'علاقه مندی',
+
+                          )
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Tooltip(message: product.title, child: Text(product.title,overflow: TextOverflow.ellipsis ,maxLines: 1,)),
+                      ),
+                      SizedBox(height: 8),
+                      Padding(
+                        padding: const EdgeInsets.only(right: 8,left: 8),
+                        child: Text(product.previousPrice.withPriceLable,style: Theme.of(context).textTheme.bodySmall?.copyWith(decoration: TextDecoration.lineThrough),),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(right: 8,left: 8,top: 4),
+                        child: Text(product.price.withPriceLable),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          ),
+        )
+      ],
+    );
+  }
+}
